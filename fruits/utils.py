6ed30c2c860d83153bcf3bf3de924b79873b9060
tufat <ugonjokubarthlomew@gmail.com>
2023-10-22 16:53:10 +0100

    apache app fro heroku

--- fruits/utils.py
+++ fruits/utils.py
@@ -1,5 +1,5 @@
 import json
-from . models import *
+from .models import *
 
 def cookieCart(request):
     try:
--- market/settings.py
+++ market/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os.path
 
+#FOR HEROKU SETTINGS
+
+import django_heroku
+import dj_database_url
+from decouple import Config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,6 +147,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 MEDIA_URL = '/images/'
@@ -165,16 +173,20 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_CONFIRMATION_COOLDOWN = 180
 ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 7
 ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300
+ACCOUNT_SIGNUP_REDIRECT_URL = 'fruits:store'
+
 
 
 LOGIN_REDIRECT_URL = 'fruits:store'
 
 
 # EMAIL SETTINGS
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST_USER = 'ugonjokubarthlomew@gmail.com'
 EMAIL_HOST_PASSWORD = 'jsekwrkspootruqf'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
+EMAIL_USE_SSL = False
+
+django_heroku.settings(locals())
